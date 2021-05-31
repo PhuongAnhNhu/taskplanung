88e519a7ac5c91b8712c174f4f09ae9922e1df71
Phương Anh Như <nhuphuonganh001@gmail.com>
2021-05-31 14:55:24 +0200

    Header

---
+++ src/component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Badge, Row } from "react-bootstrap";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="title">
+      <h1>Mein TODO-test</h1>
+    </div>
+  );
+};
+
+export default Header;
