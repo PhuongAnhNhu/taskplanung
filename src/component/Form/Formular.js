c909809fa6f9d2d94c3921fdf2d03b1fdf808d90
Phương Anh Như <nhuphuonganh001@gmail.com>
2021-06-01 14:13:14 +0200

    saveHandler to save data

--- src/component/Form/Formular.js
+++ src/component/Form/Formular.js
@@ -3,17 +3,29 @@ import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-const Formular = () => {
+const Formular = ({onNewToDo}) => {
   const [startDate, setStartDate] = useState(new Date());
+  const [task, setTask] = useState("");
+  const [kategotie, setKategotie] = useState("");
+
+  const saveHandler = () => {
+    onNewToDo({task, kategotie, startDate})
+  }
+
   return (
     <div>
       <Container fluid>
         <Row className="justify-content-md-center">
           <Col xs={12} md={6}>
             <Form>
-              <Form.Group>
+              <Form.Group controlId="aufgabe">
                 <Form.Label>Aufgabe</Form.Label>
-                <Form.Control type="text" placeholder="Do Be Do Be Do" />
+                <Form.Control
+                  type="text"
+                  placeholder="Do Be Do Be Do"
+                  value={task}
+                  onChange={(e) => setTask(e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group>
@@ -28,14 +40,20 @@ const Formular = () => {
 
               <Form.Group controlId="exampleForm.ControlSelect1">
                 <Form.Label>Kategotie</Form.Label>
-                <Form.Control as="select">
+                <Form.Control
+                  as="select"
+                  value={kategotie}
+                  onChange={(e) => setKategotie(e.target.value)}
+                >
                   <option>Privat</option>
                   <option>Arbeit</option>
                   <option>Schule</option>
                 </Form.Control>
               </Form.Group>
 
-              <Button variant="secondary">Aufgabe speichen</Button>
+              <Button variant="secondary" onClick={saveHandler}>
+                Aufgabe speichen
+              </Button>
             </Form>
           </Col>
         </Row>
