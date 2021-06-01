2dfbc2e52b50f206ed591714d9b362d3bbeee239
Phương Anh Như <nhuphuonganh001@gmail.com>
2021-06-01 14:14:01 +0200

    added tasks state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { Button } from "react-bootstrap";
+import { useState } from "react";
 import Foot from "./component/Foot/Foot";
 import Formular from "./component/Form/Formular";
 import Header from "./component/Header/Header";
 import TaskList from "./component/TaskList/TaskList";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
   return (
     <div className="App">
       <Header />
       <main className="py-3">
-        <Formular />
-        <TaskList />
+        <Formular onNewToDo={(task) =>setTasks([...tasks, task])} />
+        <TaskList tasks={tasks}/>
       </main>
       <footer>
         <Foot />
