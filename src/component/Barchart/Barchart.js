62b3619c767cc7d37444a4060d37c414a3ee1b35
Phương Anh Như <nhuphuonganh001@gmail.com>
2021-06-03 15:01:30 +0200

    readme

--- src/component/Barchart/Barchart.js
+++ src/component/Barchart/Barchart.js
@@ -16,42 +16,50 @@ const Barchart = ({ tasks }) => {
 
   useEffect(() => {
     setData(() => {
-      return tasks.reduce((currentData, task) => {
-        const dataIndex = currentData.findIndex(singleData => singleData.startDate === task.startDate);
-        if (dataIndex === -1) {
-          currentData.push({ startDate: task.startDate, count: 1 })
-        } else {
-          currentData[dataIndex].count++;
-        }
-        return [...currentData];
-      }, []).sort((a,b) => a.startDate > b.startDate ? 1: -1);
-    })
+      return tasks
+        .reduce((currentData, task) => {
+          const dataIndex = currentData.findIndex(
+            (singleData) => singleData.startDate === task.startDate
+          );
+          if (dataIndex === -1) {
+            currentData.push({ startDate: task.startDate, count: 1 });
+          } else {
+            currentData[dataIndex].count++;
+          }
+          return [...currentData];
+        }, [])
+        .sort((a, b) => (a.startDate > b.startDate ? 1 : -1));
+    });
   }, [tasks]);
 
   return (
     <Container fluid className="mt-3">
       <Row className="justify-content-md-center">
         <Col xs={12} md={6}>
-         {data.length == 0 ?  <div></div> : (<ResponsiveContainer width="100%" height={300}>
-            <BarChart
-              // width={760}
-              // height={300}
-              data={data}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-              }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="startDate" />
-              <YAxis />
-              <Tooltip />
-              <Legend />
-              <Bar dataKey="count" name="Offene Todos" fill="#8884d8" />
-            </BarChart>
-          </ResponsiveContainer>) }
+          {data.length == 0 ? (
+            <div></div>
+          ) : (
+            <ResponsiveContainer width="100%" height={300}>
+              <BarChart
+                // width={760}
+                // height={300}
+                data={data}
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="startDate" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="count" name="Offene Todos" fill="#8884d8" />
+              </BarChart>
+            </ResponsiveContainer>
+          )}
         </Col>
       </Row>
     </Container>
